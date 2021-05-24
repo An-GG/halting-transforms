489695e22d9cb763006cd6eb9cf42cb096dea7a7
Ankush Girotra <ankush.girotra@dlzpgroup.com>
2021-05-23 22:28:18 -0500

    idk

---
+++ src/ht.ts
@@ -0,0 +1,21 @@
+import { JSONSchema7 } from "json-schema";
+
+/**
+ * Define: Input and Output objects can be represented as JSON
+ * Define: Input Type and Output Type can be represented as JSON (JSONSchema7)
+ */
+
+
+let inputFormat:JSONSchema7;
+let outputFormat:JSONSchema7;
+
+let i:JSON;
+let o:JSON;
+
+type ConversionLogic = {}
+
+
+
+
+
+
